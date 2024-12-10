f8e346512ced535f434a65913b03b580d2ea1b4a
ohhyeonseo <ohs4784@gmail.com>
2024-12-11 07:44:33 +0900

    1. 이미지 안 띄워짐 2. 디테일 페이지 변경 필요 3. 수거자 리스트에도 업데이트 필요
    위 내용 수정 전 commit
    + 수거 하시겠습니까? 버튼 누르면 showDialog 됨

--- app/src/main/java/hyeonseo/ai/a2024project/AboutDetailsCollecter.java
+++ app/src/main/java/hyeonseo/ai/a2024project/AboutDetailsCollecter.java
@@ -1,5 +1,6 @@
 package hyeonseo.ai.a2024project;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,8 +8,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Gallery;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -22,6 +25,7 @@ public class AboutDetailsCollecter extends AppCompatActivity {
     Gallery gallery;
 
     Button backBtn;
+    Button successBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,12 +44,41 @@ public class AboutDetailsCollecter extends AppCompatActivity {
 
         backBtn = findViewById(R.id.back_button);
 
+        successBtn = findViewById(R.id.collecter_success);
+
+        successBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog();
+            }
+        });
+
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
+    }
+
+    void showDialog(){
+        AlertDialog.Builder msgBuilder = new AlertDialog.Builder(AboutDetailsCollecter.this)
+                .setTitle("수거 수락")
+                .setMessage("해당 약물을 수거 하시겠습니까?")
+                .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(AboutDetailsCollecter.this, "수거 수락 완료", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(AboutDetailsCollecter.this, "수거 수락 취소", Toast.LENGTH_SHORT).show();
+                    }
+                });
+        AlertDialog msgDlg = msgBuilder.create();
+        msgDlg.show();
     }
 
 
